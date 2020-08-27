2b6d7b0ee7f6713b9db332c6fc0ad8fc7e2dbabc
Natreton <natreton@abv.bg>
2020-08-27 21:47:21 +0300

    Update LocalisationMethods.java

--- LocalisationMethods.java
+++ LocalisationMethods.java
@@ -18,15 +18,70 @@ public class LocalisationMethods {
 						System.out.println("Text is not correct!");
 					}
 				}
-				
-				public static void find_captionField(WebDriver driver) {
-					WebElement findCaptionField = driver.findElement(By.xpath("//input[@placeholder='Enter you post caption here']"));
-					boolean displayed = findCaptionField.isDisplayed();
-					assertTrue(displayed);
-				}
+			
+			
 				
 				public static void find_singinButton(WebDriver driver) {
 					WebElement findSignIn = driver.findElement(By.xpath("//button[@id='sign-in-button']"));
 					findSignIn.click();
+				}
+				
+				public static void find_dropdownSearchName(WebDriver driver) {
+					WebElement findSearchName = driver.findElement(By.xpath("//a[contains(text(),'Emile Hutton')]"));
+					if (findSearchName.isDisplayed()) {
+						findSearchName.click();
+					}
+					else {
+						System.out.println("Name is not correct");
+					}
+					
+				}
+				public static void find_thecaptionField(WebDriver driver) {
+					WebElement findtheCaptionField = driver.findElement(By.xpath("//input[@placeholder='Enter you post caption here']"));
+					boolean displayed = findtheCaptionField.isDisplayed();
+					assertTrue(displayed);
+					//assertTrue(displayed, "Enter you post caption here");
+				}
+				public static void find_SearchName(WebDriver driver) {
+					WebElement findDropdownName = driver.findElement(By.xpath("//div[@class='dropdown-container']//a[contains(text(),'Emile Hutton')]"));
+					boolean displayed = findDropdownName.isDisplayed();
+					assertTrue(displayed);
+					//assertTrue(displayed,"Emile Hutton");
+				}
+				public static void find_filterprivateDisabled(WebDriver driver) {
+					WebElement privateDisabled = driver.findElement(By.xpath("//label[@class='btn-private btn btn-primary disabled']"));
+					boolean displayed = privateDisabled.isDisplayed();
+					assertTrue(displayed);
+				}
+				public static void find_buttonFollow(WebDriver driver) {
+					WebElement findFollow = driver.findElement(By.xpath("//button[contains(text(),'Follow')]"));
+					boolean displayed = findFollow.isDisplayed();
+					assertTrue(displayed);
+				}
+				
+				public static void find_NoPostHereElements(WebDriver driver) {
+					WebElement findImageFace = driver.findElement(By.xpath("//i[contains(@class,'fas fa-sad-tear  fa-3x')]"));
+					boolean displayedImage = findImageFace.isDisplayed();
+					assertTrue(displayedImage);
+					WebElement nopostHere = driver.findElement(By.xpath("//h3[contains(text(),'No posts here')]"));
+					boolean displayedText = nopostHere.isDisplayed();
+					assertTrue(displayedText);
+				}
+				
+				public static void find_buttonX_closeForm(WebDriver driver) {
+					WebElement findXClose = driver.findElement(By.xpath("//i[contains(@class,'fas fa-times')]"));
+					findXClose.click();
+				}
+				
+				
+				public static void find_followingCounter(WebDriver driver) {
+					WebElement findCounter = driver.findElement(By.id("following"));
+					findCounter.click();
+				}
+				
+				public static void find_FormFollowing(WebDriver driver) {
+					WebElement formFollowing = driver.findElement(By.xpath("//div[contains(@class,'modal-content')]//app-followers-modal"));
+					boolean display = formFollowing.isDisplayed();
+					assertTrue(display);
 				}
 }
