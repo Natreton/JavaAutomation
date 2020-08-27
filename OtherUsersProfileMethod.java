43858791c6d080e811f205168e20a0798c514ece
Natreton <natreton@abv.bg>
2020-08-27 13:30:11 +0300

    Update OtherUsersProfileMethod.java

--- OtherUsersProfileMethod.java
+++ OtherUsersProfileMethod.java
@@ -1,5 +1,7 @@
 package webdriver;
 
+import static org.junit.Assert.assertTrue;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,5 +10,10 @@ public class OtherUsersProfileMethod {
 				public static void accessbyName(WebDriver driver) {
 					WebElement finduserName = driver.findElement(By.xpath("//a[contains(text(),'Georgi_Stankov')]"));
 					finduserName.click();
+				}
+				public static void find_other_user_private_image(WebDriver driver) {
+					WebElement imagePrivate = driver.findElement(By.xpath("(//app-post//div[@class='post-img']//img)[1]"));
+					boolean displayed = imagePrivate.isDisplayed();
+					assertTrue(displayed);
 				}
 }
